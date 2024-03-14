212c37b1629af6b0bb6693c2c38c57cdba2e69d7
Oluwanifemi Ogidama <ogidamanifemi@gmail.com>
2024-03-14 12:47:53 -0400

    removed duplicate project

--- CroKnitters/CroKnitters/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./Views/**/*.{cshtml,razor}", "./wwwroot.js/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
