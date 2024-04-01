b9acf83cdc50b1d923c2b7ad948e19f70c43257a
Oluwanifemi Ogidama <ogidamanifemi@gmail.com>
2024-04-01 15:23:06 -0400

    Added Preference Services

---
+++ CroKnitters/Services/IUserPreferenceService.cs
@@ -0,0 +1,11 @@
+﻿using CroKnitters.Entities;
+using System.Linq;
+
+namespace CroKnitters.Services
+{
+    public interface IUserPreferenceService
+    {
+        Preference GetUserPreference(int userId);
+        void UpdateUserPreference(int userId, int languageId, int themeId);
+    }
+}
---
+++ CroKnitters/Services/UserPreferenceService.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using CroKnitters.Entities;
+
+namespace CroKnitters.Services
+{
+    public class UserPreferenceService : IUserPreferenceService
+    {
+        private readonly CrochetAppDbContext _context;
+
+        public UserPreferenceService(CrochetAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Preference GetUserPreference(int userId)
+        {
+            return _context.Preferences.FirstOrDefault(p => p.UserId == userId);
+        }
+
+        public void UpdateUserPreference(int userId, int languageId, int themeId)
+        {
+            var preference = _context.Preferences.FirstOrDefault(p => p.UserId == userId);
+
+            if (preference == null)
+            {
+                preference = new Preference
+                {
+                    UserId = userId,
+                    LanguageId = languageId,
+                    ThemeId = themeId
+                };
+                _context.Preferences.Add(preference);
+            }
+            else
+            {
+                preference.LanguageId = languageId;
+                preference.ThemeId = themeId;
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
